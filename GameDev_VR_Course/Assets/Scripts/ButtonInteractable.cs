bb5f02646c538744fc7cfb76a73a451106e19ad7
JeremyRoalef <roalef944@morrisville.edu>
2025-03-02 15:09:05 -0500

    4.4 Converting the Button to an Interactable
    Key Takeaways: Change the Left & Right UI Controllers' Select Action and Select Action Value to the UI Press and Press Value presets. This needs to be done since the button component was removed from the button and the previous presets were set to the TP presets, not UI presets.

---
+++ GameDev_VR_Course/Assets/Scripts/ButtonInteractable.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ButtonInteractable : XRSimpleInteractable
+{
+    [SerializeField]
+    Color[] buttonColors = new Color[4];
+
+    [SerializeField]
+    Image buttonImage;
+
+    Color buttonNormalColor;
+    Color buttonHighlightedColor;
+    Color buttonPressedColor;
+    Color buttonSelectedColor;
+
+    bool isPressed;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        //Set the colors of the different button states
+        buttonNormalColor = buttonColors[0];
+        buttonHighlightedColor = buttonColors[1];
+        buttonPressedColor = buttonColors[2];
+        buttonSelectedColor = buttonColors[3];
+
+        //Set image color to normal color
+        buttonImage.color = buttonNormalColor;
+
+    }
+
+    //Controller hovered over button
+    protected override void OnHoverEntered(HoverEnterEventArgs args)
+    {
+        base.OnHoverEntered(args);
+        isPressed = false;
+
+        buttonImage.color = buttonHighlightedColor;
+    }
+
+    //Controller stopped hovering over button
+    protected override void OnHoverExited(HoverExitEventArgs args)
+    {
+        base.OnHoverExited(args);
+
+        if (isPressed) { return; }
+        buttonImage.color = buttonNormalColor;
+    }
+
+    //Button has been selected
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+
+        if (isPressed) { return; }
+        isPressed = true;
+        buttonImage.color = buttonPressedColor;
+    }
+    
+    //Button is no longer selected
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        buttonImage.color = buttonSelectedColor;
+    }
+}
